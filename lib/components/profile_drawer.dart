3da75edef27a655add32d27e9285a9a7e17467ad
DH <shim5013@naver.com>
2023-03-29 11:40:56 +0900

    완료

---
+++ lib/components/profile_drawer.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ProfileDrawer extends StatelessWidget {
+  const ProfileDrawer({super.key,});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      color: Colors.blue,
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme(){
+  return ThemeData(
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.white,
+      foregroundColor: Colors.black,
+      iconTheme: IconThemeData(
+        color: Colors.blue
+      )
+    ),
+  );
+}
